e649d78800cbb530cbac8aea01b45f0a66683aef
MohammedAbidNafi <abid.nafi04@gmail.com>
2021-01-28 09:15:20 +0530

    Added Crashlytics

--- app/src/main/java/com/margsapp/messenger/StartActivity.java
+++ app/src/main/java/com/margsapp/messenger/StartActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -16,6 +18,8 @@ public class StartActivity extends AppCompatActivity {
     AppCompatButton login,register;
     
     FirebaseUser firebaseUser;
+
+
 
 
     @Override
@@ -35,6 +39,8 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
 
 
 
